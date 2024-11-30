af464b91f4efa41fec6f30b03c24c87916d6b3b1
SeungYeon04 <100332811+SeungYeon04@users.noreply.github.com>
2024-11-30 14:00:13 +0900

    .

--- src/main3.js
+++ src/main3.js
@@ -36,7 +36,7 @@ class App {
     const width = window.innerWidth; 
     const height = window.innerHeight; 
     const camera = new THREE.PerspectiveCamera(60, width / height, 0.1, 100); //60화각이 작을수록 외곡이 적다 
-    camera.position.set(2, 2, 3.5); 
+    camera.position.set(25, 1, 3.5); 
     this._camera = camera; 
   }
 
@@ -54,55 +54,85 @@ class App {
   }
 
   _setupModel() {
-    const axishelper = new THREE.AxesHelper(3); 
-    this._scene.add(axishelper); //z파랑 y초록 z빨강 
-    this._axishelper = axishelper;
-    axishelper.position.y = -1; 
+    const geometry_sun = new THREE.SphereGeometry(3); 
+    const geometry_susung = new THREE.SphereGeometry(0.5); 
+    const geometry_moon = new THREE.SphereGeometry(1); 
+    const geometry_gigu = new THREE.SphereGeometry(0.9); 
+    const geometry_mocsung = new THREE.SphereGeometry(1.9); 
+    const geometry_tosung = new THREE.SphereGeometry(1.2); 
+    const material_1 = new THREE.MeshStandardMaterial({color: "gray"}); 
+    const material_2 = new THREE.MeshStandardMaterial({color: "red"}); 
+    const material_3 = new THREE.MeshStandardMaterial({color: "blue", flatShading: true}); 
+    const material_4 = new THREE.MeshStandardMaterial({color: "yellow"}); 
 
-    const material = new THREE.MeshStandardMaterial({color: "#aaaaaa"}); 
-    const gromGround = new THREE.PlaneGeometry(5, 5); 
-    const ground = new THREE.Mesh(gromGround, material); 
-    this._scene.add(ground); //소환 
-    ground.rotation.x = THREE.MathUtils.degToRad(-90); 
-    ground.position.y = -1; 
-    
 
-    const geomBigSphere = new THREE.SphereGeometry(1, 32, 16,0, THREE.MathUtils.degToRad(360),0, THREE.MathUtils.degToRad(90)); 
-    const bigSphere = new THREE.Mesh(geomBigSphere, material); 
-    this._scene.add(bigSphere); //소환
-    bigSphere.position.y = -1; 
-  
-    const geomSmallSphere = new THREE.SphereGeometry(0.2);
-    const smallSphere = new THREE.Mesh(geomSmallSphere, material); 
-    const smallSpherePivot = new THREE.Object3D(); 
-    bigSphere.add(smallSpherePivot); //부모로 지정 
-    smallSpherePivot.add(smallSphere); //자식으로 지정 
-    smallSphere.position.x = -2; 
-    smallSphere.position.y = 0.1; 
+    const Sun = new THREE.Mesh(geometry_sun, material_2); 
+    this._scene.add(Sun);
+    this._sun = Sun;
 
-    this._smallSpherePivot = smallSpherePivot;
-    this._smallSphere = smallSphere; 
+    const Susung = new THREE.Mesh(geometry_susung, material_1); //선언 -> 모양,크기 + 색상 
+    this._scene.add(Susung); //생성 
+    Susung.position.x = 3.5; 
+    this._gigu = Susung; //필드화 
+    Sun.add(Susung);
 
-    const cntItems = 8; 
-    const geomTorus = new THREE.TorusGeometry(0.2, 0.1); 
-    for(let i = 0; i < cntItems; i++){
-      const torus = new THREE.Mesh(geomTorus, material); 
-      const torusPivot = new THREE.Object3D(); 
-      torusPivot.add(torus); 
-      bigSphere.add(torusPivot); 
-      torusPivot.rotation.y = THREE.MathUtils.degToRad(360) * i / cntItems; 
-      torus.position.x = 2; //부모 -> 자식 기준으로 돈다 
-    }
-    
-    
+    const Gigu = new THREE.Mesh(geometry_gigu, material_3); //선언 -> 모양,크기 + 색상 
+    this._scene.add(Gigu); //생성 
+    Gigu.position.x = 7; 
+    this._gigu = Gigu; //필드화 
+    Sun.add(Gigu); //부모요소 추가 
+
+    const Gmesung = new THREE.Mesh(geometry_susung, material_4); //선언 -> 모양,크기 + 색상 
+    this._scene.add(Gmesung); //생성 
+    Gmesung.position.x = 5; 
+    this._gigu = Gmesung; //필드화 
+    Sun.add(Gmesung);
+
+    const Hawsung = new THREE.Mesh(geometry_susung, material_2); //선언 -> 모양,크기 + 색상 
+    this._scene.add(Hawsung); //생성 
+    Hawsung.position.x = 9; 
+    this._gigu = Hawsung; //필드화 
+    Sun.add(Hawsung);
+
+    const Mocsung = new THREE.Mesh(geometry_mocsung, material_4); 
+    this._scene.add(Mocsung);
+    Mocsung.position.x = 12; 
+    this._moon = Mocsung; 
+    Sun.add(Mocsung);
+
+    const Tosung = new THREE.Mesh(geometry_tosung, material_4); 
+    this._scene.add(Tosung);
+    Tosung.position.x = 16; 
+    this._moon = Tosung; 
+    Sun.add(Tosung);
+
+    const Chun = new THREE.Mesh(geometry_gigu, material_3); 
+    this._scene.add(Chun);
+    Chun.position.x = 18; 
+    this._moon = Chun; 
+    Sun.add(Chun);
+
+    const Hea = new THREE.Mesh(geometry_gigu, material_3); 
+    this._scene.add(Hea);
+    Hea.position.x = 21; 
+    this._moon = Hea; 
+    Sun.add(Hea);
+
+    //const Moon = new THREE.Mesh(geometry_moon, material_1); 
+    //this._scene.add(Moon);
+    //Moon.position.x = 30; 
+    //this._moon = Moon; 
+    //Sun.add(Moon);
   }
 
   update() {
     const delta = this._clock.getDelta();
     
-    this._smallSpherePivot.rotation.y += delta; 
-    this._axishelper.rotation.y += delta; 
+    this._sun.rotation.y += delta;
+    this._gigu.rotation.y += delta;
+    this._moon.rotation.y += delta;
     this._controls.update();
+
   }
   
   render(){
